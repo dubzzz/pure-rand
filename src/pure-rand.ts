fe070b8afd9eb22b1003839ed47d144fbca45544
Nicolas DUBIEN <github@dubien.org>
2018-03-02 20:39:08 +0100

    Export Distribution related elements

--- src/pure-rand.ts
+++ src/pure-rand.ts
@@ -2,7 +2,14 @@ import { RandomGenerator, generate_n, skip_n } from './generator/RandomGenerator
 import congruential from './generator/LinearCongruential';
 import mersenne from './generator/MersenneTwister';
 
+import Distribution from './distribution/Distribution';
+import { uniformIntDistribution } from './distribution/UniformDistribution';
+
 export {
+    // RandomGenerator
     RandomGenerator, generate_n, skip_n,
-    congruential, mersenne
+    congruential, mersenne,
+    // Distribution
+    Distribution,
+    uniformIntDistribution
 };
