d500ce95ac320480dcae3abe70b05b96dc3f977f
Nicolas DUBIEN <github@dubien.org>
2018-03-06 01:04:02 +0100

    Export and export default should be the same

--- src/pure-rand-default.ts
+++ src/pure-rand-default.ts
@@ -1,13 +1,14 @@
-import { generate_n, skip_n } from './generator/RandomGenerator';
+import { RandomGenerator, generate_n, skip_n } from './generator/RandomGenerator';
 import congruential from './generator/LinearCongruential';
 import mersenne from './generator/MersenneTwister';
 
+import Distribution from './distribution/Distribution';
 import { uniformIntDistribution } from './distribution/UniformDistribution';
 
 export {
-    // RandomGenerator related
+    RandomGenerator,
     generate_n, skip_n,
     congruential, mersenne,
-    // Distribution related
+    Distribution,
     uniformIntDistribution
 };
--- src/pure-rand.ts
+++ src/pure-rand.ts
@@ -2,6 +2,3 @@ import * as prand from './pure-rand-default';
 export default prand;
 
 export * from './pure-rand-default';
-import RandomGenerator from './generator/RandomGenerator';
-import Distribution from './distribution/Distribution';
-export { RandomGenerator, Distribution };
