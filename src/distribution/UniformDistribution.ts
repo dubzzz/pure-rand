6e611cb54edc99b67edbf900336490512c849ba1
Nicolas DUBIEN <github@dubien.org>
2018-05-21 20:52:53 +0200

    Extract uniformHelper from uniformIntDistribution

--- src/distribution/UniformDistribution.ts
+++ src/distribution/UniformDistribution.ts
@@ -3,19 +3,20 @@ import RandomGenerator from '../generator/RandomGenerator';
 
 const uniformIntDistribution = (from: number, to: number): Distribution<number> => {
     const diff = to - from +1;
+    function uniformHelper(rng: RandomGenerator, MIN_RNG: number, MAX_ALLOWED: number): [number, RandomGenerator] {
+        let nrng = rng;
+        while (true) {
+            const [v, tmpRng] = nrng.next();
+            const deltaV = v - MIN_RNG;
+            nrng = tmpRng;
+            return [deltaV % diff + from, nrng]
+        }
+    };
     function helper(rng: RandomGenerator): [number, RandomGenerator] {
         const MIN_RNG = rng.min();
         const NUM_VALUES = rng.max() - rng.min() +1;
         const MAX_ALLOWED = NUM_VALUES - (NUM_VALUES % diff);
-
-        let nrng = rng;
-        let deltaV = MAX_ALLOWED;
-        while (deltaV >= MAX_ALLOWED) {
-            const [v, tmpRng] = nrng.next();
-            nrng = tmpRng;
-            deltaV = v - MIN_RNG;
-        }
-        return [deltaV % diff + from, nrng];
+        return uniformHelper(rng, MIN_RNG, MAX_ALLOWED);
     }
     return helper;
 };
