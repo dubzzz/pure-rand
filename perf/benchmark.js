f8480e8cdac115877358497725b22727815810f3
Nicolas DUBIEN <github@dubien.org>
2018-10-06 00:30:37 +0200

    More cycles for benchmarks

--- perf/benchmark.js
+++ perf/benchmark.js
@@ -5,39 +5,43 @@
 // $:  tsc --target es6 --outDir "lib-new/"
 // $:  node perf/benchmark.js
 const { genFor } = require('./helpers');
-const { testGenerateWithSameDistribution, testGenerateWithSkipDistribution } = require('./tasks');
+const { testGenerateWithSameDistribution, testGenerateWithSkipDistributionSingle } = require('./tasks');
 const Benchmark = require('benchmark');
 const prandRef = require('../lib/pure-rand');
 const prandTest = require('../lib-new/pure-rand');
 
-const WARMUP_SAMPLES = 10000;
-const MIN_SAMPLES = 10000;
-const NUM_TESTS = 1000;
+const WARMUP_SAMPLES = 100;
+const MIN_SAMPLES = 1000;
+const benchConf = { initCount: WARMUP_SAMPLES, minSamples: MIN_SAMPLES };
+
+const NUM_TESTS = 100;
 const PROF_GEN = process.env.PROF_GEN || 'congruential32';
 console.log(`Generator....: ${PROF_GEN}\n`);
 
-(new Benchmark.Suite())
-    .add('Reference', () => {
-        const g = genFor(prandRef, PROF_GEN);
-        testGenerateWithSkipDistribution(prandRef, g, NUM_TESTS);
-    })
-    .add('Test', () => {
-        const g = genFor(prandTest, PROF_GEN);
-        testGenerateWithSkipDistribution(prandTest, g, NUM_TESTS);
-    })
-    .on('cycle', (event) => console.log(String(event.target)))
-    .on('complete', function() { console.log(`With skip distribution: ${this.filter('fastest').map('name')}\n`); })
-    .run({ initCount: WARMUP_SAMPLES, minSamples: MIN_SAMPLES });
+const buildBenchmarks = (type, lib) => {
+    return [
+        new Benchmark(
+            `skip@${type}`,
+            () => {
+                const g = genFor(lib, PROF_GEN);
+                testGenerateWithSkipDistributionSingle(prandRef, g);
+            }, benchConf),
+        new Benchmark(
+            `same@${type}`,
+            () => {
+                const g = genFor(lib, PROF_GEN);
+                testGenerateWithSameDistribution(prandRef, g, NUM_TESTS);
+            }, benchConf)
+    ];
+};
 
-(new Benchmark.Suite())
-    .add('Reference', () => {
-        const g = genFor(prandRef, PROF_GEN);
-        testGenerateWithSameDistribution(prandRef, g, NUM_TESTS);
-    })
-    .add('Test', () => {
-        const g = genFor(prandTest, PROF_GEN);
-        testGenerateWithSameDistribution(prandTest, g, NUM_TESTS);
-    })
-    .on('cycle', (event) => console.log(String(event.target)))
-    .on('complete', function() { console.log(`With same distribution: ${this.filter('fastest').map('name')}\n`); })
-    .run({ initCount: WARMUP_SAMPLES, minSamples: MIN_SAMPLES });
+Benchmark.invoke(
+    [
+        ...buildBenchmarks('Reference', prandRef),
+        ...buildBenchmarks('Test', prandTest)
+    ], {
+        name: 'run',
+        queued: true,
+        onCycle: (event) => console.log(String(event.target)),
+    }
+);
--- perf/tasks.js
+++ perf/tasks.js
@@ -1,9 +1,12 @@
 // @ts-check
 
 // Avoid the construction of an intermediate distribution
+exports.testGenerateWithSkipDistributionSingle = (lib, g) => {
+    return lib.uniformIntDistribution(0, 0xffffffff, g)[1];
+};
 exports.testGenerateWithSkipDistribution = (lib, g, NUM_TESTS) => {
     for (let idx = 0 ; idx !== NUM_TESTS ; ++idx) {
-        g = lib.uniformIntDistribution(0, 0xffffffff, g)[1];
+        g = exports.testGenerateWithSkipDistributionSingle(lib, g);
     }
     return g;
 };
