cba3db21cc4ce21c251802ff634851942fe954fe
Nicolas DUBIEN <github@dubien.org>
2020-01-17 20:27:51 +0100

    Improve performances of xoroshiro128+

--- src/generator/XoroShiro.ts
+++ src/generator/XoroShiro.ts
@@ -11,21 +11,16 @@ class XoroShiro128Plus implements RandomGenerator {
   max(): number {
     return 0x7fffffff;
   }
-  private rotl0(n1: number, n0: number, k: number) {
-    if (k < 32) return (n0 << k) | (n1 >>> (32 - k));
-    return (n1 << (k - 32)) | (n0 >>> (64 - k));
-  }
-  private rotl1(n1: number, n0: number, k: number) {
-    if (k < 32) return (n1 << k) | (n0 >>> (32 - k));
-    return (n0 << (k - 32)) | (n1 >>> (64 - k));
-  }
   next(): [number, RandomGenerator] {
+    // a = s0[n] ^ s1[n]
     const a0 = this.s10 ^ this.s00;
     const a1 = this.s11 ^ this.s01;
-    const ns00 = this.rotl0(this.s01, this.s00, 24) ^ a0 ^ (a0 << 16);
-    const ns01 = this.rotl1(this.s01, this.s00, 24) ^ a1 ^ ((a1 << 16) | (a0 >>> 16));
-    const ns10 = this.rotl0(a1, a0, 37);
-    const ns11 = this.rotl1(a1, a0, 37);
+    // s0[n+1] = rotl(s0[n], 24) ^ a ^ (a << 16)
+    const ns00 = (this.s00 << 24) ^ (this.s01 >>> 8) ^ a0 ^ (a0 << 16);
+    const ns01 = (this.s01 << 24) ^ (this.s00 >>> 8) ^ a1 ^ ((a1 << 16) | (a0 >>> 16));
+    // s1[n+1] = rotl(a, 37)
+    const ns10 = (a1 << 5) ^ (a0 >>> 27);
+    const ns11 = (a0 << 5) ^ (a1 >>> 27);
     return [(this.s00 + this.s10) | 0, new XoroShiro128Plus(ns01, ns00, ns11, ns10)];
   }
 }
