6cacea23b7052c629540f4bc376e385114484177
Nicolas DUBIEN <github@dubien.org>
2020-01-21 22:58:29 +0100

    Export xoroshiro128plus

--- src/pure-rand-default.ts
+++ src/pure-rand-default.ts
@@ -2,6 +2,7 @@ import { RandomGenerator, generateN, skipN } from './generator/RandomGenerator';
 import { congruential, congruential32 } from './generator/LinearCongruential';
 import mersenne from './generator/MersenneTwister';
 import { xorshift128plus } from './generator/XorShift';
+import { xoroshiro128plus } from './generator/XoroShiro';
 
 import Distribution from './distribution/Distribution';
 import { uniformBigIntDistribution } from './distribution/UniformBigIntDistribution';
@@ -15,6 +16,7 @@ export {
   congruential32,
   mersenne,
   xorshift128plus,
+  xoroshiro128plus,
   Distribution,
   uniformBigIntDistribution,
   uniformIntDistribution
