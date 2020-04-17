3017476bd8024574cc2d0b42f3a31b2c0a38d173
Nicolas DUBIEN <github@dubien.org>
2020-04-17 20:56:01 +0200

    More flags in profiler.cjs

--- perf/profiler.cjs
+++ perf/profiler.cjs
@@ -2,7 +2,7 @@
 // This file is a sample snippet to run a profiler on
 // Run it:
 // $:  tsc --target es6
-// $:  PROF_TYPE=next PROF_GEN=xoroshiro128plus node --prof --no-logfile-per-isolate perf/profiler.cjs
+// $:  PROF_TYPE=next PROF_GEN=xoroshiro128plus node --prof --no-logfile-per-isolate --trace-deopt --trace-opt-verbose perf/profiler.cjs
 // $:  node --prof-process v8.log > v8.out
 const { genFor } = require('./helpers.cjs');
 const {
