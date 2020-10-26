670e2ab7eb12eede5da74c3031c29a3fb0f39488
Nicolas DUBIEN <github@dubien.org>
2020-10-26 20:47:42 +0100

    📝 Add JSDoc on public code in /distribution (#115)

--- src/distribution/UniformIntDistribution.ts
+++ src/distribution/UniformIntDistribution.ts
@@ -45,7 +45,24 @@ function uniformIntInternal(from: number, diff: number, rng: RandomGenerator): [
   }
 }
 
+/**
+ * Uniformly generate random integer values between `from` (included) and `to` (included)
+ *
+ * @param from - Lower bound of the range (included)
+ * @param to - Upper bound of the range (included)
+ *
+ * @public
+ */
 function uniformIntDistribution(from: number, to: number): Distribution<number>;
+/**
+ * Uniformly generate random integer values between `from` (included) and `to` (included)
+ *
+ * @param from - Lower bound of the range (included)
+ * @param to - Upper bound of the range (included)
+ * @param rng - Instance of RandomGenerator to extract random values from
+ *
+ * @public
+ */
 function uniformIntDistribution(from: number, to: number, rng: RandomGenerator): [number, RandomGenerator];
 function uniformIntDistribution(from: number, to: number, rng?: RandomGenerator) {
   const diff = to - from + 1;
