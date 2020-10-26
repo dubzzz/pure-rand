c55f987015fbcb50fcc09cdba8e2f799cc2df252
Nicolas DUBIEN <github@dubien.org>
2020-10-26 20:58:56 +0100

    ♻️ Extract internal logic of uniformIntDistribution into shared internals (#116)

--- test/unit/distribution/UniformIntDistribution.spec.ts
+++ test/unit/distribution/UniformIntDistribution.spec.ts
@@ -1,148 +1,116 @@
 import * as fc from 'fast-check';
+import { mocked } from 'ts-jest/utils';
 
 import { uniformIntDistribution } from '../../../src/distribution/UniformIntDistribution';
-import mersenne from '../../../src/generator/MersenneTwister';
 import RandomGenerator from '../../../src/generator/RandomGenerator';
 
-const MERSENNE_MIN = mersenne(0).min();
-const MERSENNE_MAX = mersenne(0).max();
+import * as UniformIntDistributionInternalMock from '../../../src/distribution/internals/UniformIntDistributionInternal';
+jest.mock('../../../src/distribution/internals/UniformIntDistributionInternal');
 
-class NatGenerator implements RandomGenerator {
-  readonly current: number;
-  constructor(current: number) {
-    this.current = current % 0x80000000;
-  }
-
-  next(): [number, RandomGenerator] {
-    return [this.current, new NatGenerator(this.current + 1)];
-  }
-  min(): number {
-    return 0;
-  }
-  max(): number {
-    return 0x7fffffff;
-  }
+function buildUniqueRng() {
+  return {} as RandomGenerator;
+}
+function clean() {
+  jest.resetAllMocks();
+  jest.clearAllMocks();
 }
 
-class ModNatGenerator implements RandomGenerator {
-  constructor(readonly current: RandomGenerator, readonly mod: number) {}
-
-  next(): [number, RandomGenerator] {
-    const [v, nrng] = this.current.next();
-    return [Math.abs(v % this.mod), new ModNatGenerator(nrng, this.mod)];
-  }
-  min(): number {
-    return 0;
-  }
-  max(): number {
-    return this.mod - 1;
-  }
-}
-
-const MAX_RANGE: number = 1000;
-
+beforeEach(clean);
 describe('uniformIntDistribution', () => {
-  it('Should always generate values within the range', () =>
+  it('Should call uniformIntDistributionInternal with correct size of range and source rng', () =>
     fc.assert(
-      fc.property(fc.nat(), fc.integer(), fc.integer(0, MAX_RANGE), (offset, from, gap) => {
-        const [v, nrng] = uniformIntDistribution(from, from + gap)(new NatGenerator(offset));
-        return v >= from && v <= from + gap;
-      })
+      fc
+        .property(settingsArbitrary, (settings) => {
+          // Arrange
+          const { from, to, rng, uniformIntDistributionInternal } = mockInternals(settings);
+
+          // Act
+          uniformIntDistribution(from, to)(rng);
+
+          // Assert
+          expect(uniformIntDistributionInternal).toHaveBeenCalledTimes(1);
+          expect(uniformIntDistributionInternal).toHaveBeenCalledWith(to - from + 1, rng);
+        })
+        .beforeEach(clean)
     ));
-  it('Should be able to generate all values within the range', () =>
+
+  it('Should offset by "from" the value produced by uniformIntDistributionInternal', () =>
     fc.assert(
-      fc.property(fc.nat(), fc.integer(), fc.integer(0, MAX_RANGE), fc.nat(), (offset, from, gap, targetOffset) => {
-        const target = from + (targetOffset % (gap + 1));
-        let rng: RandomGenerator = new NatGenerator(offset);
-        for (let numTries = 0; numTries < 2 * gap + 1; ++numTries) {
-          const [v, nrng] = uniformIntDistribution(from, from + gap)(rng);
-          rng = nrng;
-          if (v === target) {
-            return true;
-          }
-        }
-        return false; //twice the length should always be enough (+1 to avoid length = 0)
-      })
+      fc
+        .property(settingsArbitrary, (settings) => {
+          // Arrange
+          const { from, to, rng, outputs } = mockInternals(settings);
+
+          // Act
+          const [v, _nrng] = uniformIntDistribution(from, to)(rng);
+
+          // Assert
+          expect(v).toBe(outputs[0][0] + from);
+        })
+        .beforeEach(clean)
     ));
-  it('Should be evenly distributed over the range', () =>
+
+  it('Should return the rng produced by uniformIntDistributionInternal', () =>
     fc.assert(
-      fc.property(fc.nat(), fc.integer(), fc.integer(0, MAX_RANGE), fc.integer(1, 100), (offset, from, gap, num) => {
-        let buckets = [...Array(gap + 1)].map(() => 0);
-        let rng: RandomGenerator = new NatGenerator(offset);
-        for (let numTries = 0; numTries < num * (gap + 1); ++numTries) {
-          const [v, nrng] = uniformIntDistribution(from, from + gap)(rng);
-          rng = nrng;
-          buckets[v - from] += 1;
-        }
-        return buckets.every((n) => n === num);
-      })
+      fc
+        .property(settingsArbitrary, (settings) => {
+          // Arrange
+          const { from, to, rng, outputs } = mockInternals(settings);
+
+          // Act
+          const [_v1, nrng] = uniformIntDistribution(from, to)(rng);
+
+          // Assert
+          expect(nrng).toBe(outputs[0][1]);
+        })
+        .beforeEach(clean)
     ));
-  it('Should be able to generate values larger than the RandomGenerator', () =>
-    fc.assert(
-      fc.property(fc.integer(), fc.integer(2, 0x7fffffff), (seed, mod) => {
-        let rng: RandomGenerator = new ModNatGenerator(mersenne(seed), mod);
-        for (let numTries = 0; numTries < 100; ++numTries) {
-          const [v, nrng] = uniformIntDistribution(Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)(rng);
-          rng = nrng;
-          if (v > rng.max()) {
-            return true;
-          }
-        }
-        return false;
-      })
-    ));
-  it('Should be able to generate values outside bitwise operations', () =>
-    fc.assert(
-      fc.property(fc.integer(), fc.integer(2, 0x7fffffff), (seed, mod) => {
-        let rng: RandomGenerator = new ModNatGenerator(mersenne(seed), mod);
-        for (let numTries = 0; numTries < 100; ++numTries) {
-          const [v, nrng] = uniformIntDistribution(Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)(rng);
-          rng = nrng;
-          if (v > 0xffffffff) {
-            return true;
-          }
-        }
-        return false;
-      })
-    ));
+
   it('Should be equivalent to call the 2-parameter and 3-parameter', () =>
     fc.assert(
-      fc.property(fc.nat(), fc.integer(), fc.integer(0, MAX_RANGE), (offset, from, gap) => {
-        const [v1, nrng1] = uniformIntDistribution(from, from + gap)(new NatGenerator(offset));
-        const [v2, nrng2] = uniformIntDistribution(from, from + gap, new NatGenerator(offset));
-        return v1 === v2;
-      })
+      fc
+        .property(settingsArbitrary, (settings) => {
+          // Arrange
+          const { from, to, rng } = mockInternals(settings);
+
+          // Act
+          const [v1, _nrng1] = uniformIntDistribution(from, to)(rng);
+          const [v2, _nrng2] = uniformIntDistribution(from, to, rng);
+
+          // Assert
+          expect(v1).toBe(v2);
+        })
+        .beforeEach(clean)
     ));
-  it.each`
-    from                       | to                         | topic
-    ${0}                       | ${2 ** 3 - 1}              | ${"range of size divisor of mersenne's one"}
-    ${0}                       | ${2 ** 3 - 2}              | ${"range of size divisor of mersenne's one minus one"}
-    ${0}                       | ${2 ** 3}                  | ${"range of size divisor of mersenne's one plus one"}
-    ${48}                      | ${69}                      | ${'random range'}
-    ${MERSENNE_MIN}            | ${MERSENNE_MAX}            | ${"mersenne's range"}
-    ${MERSENNE_MIN}            | ${MERSENNE_MAX - 1}        | ${"mersenne's range minus one"}
-    ${MERSENNE_MIN}            | ${MERSENNE_MAX + 1}        | ${"mersenne's range plus one"}
-    ${0}                       | ${2 ** 40 - 1}             | ${"range of size multiple of mersenne's one"}
-    ${0}                       | ${2 ** 40 - 2}             | ${"range of size multiple of mersenne's one minus one"}
-    ${0}                       | ${2 ** 40}                 | ${"range of size multiple of mersenne's one plus one"}
-    ${Number.MIN_SAFE_INTEGER} | ${Number.MAX_SAFE_INTEGER} | ${'full integer range'}
-  `('Should not change its output in range ($from, $to) except for major bumps', ({ from, to }) => {
-    // Remark:
-    // ========================
-    // This test is purely there to ensure that we do not introduce any regression
-    // during a commit without noticing it.
-    // The values we expect in the output are just a snapshot taken at a certain time
-    // in the past. They might be wrong values with bugs.
-
-    let rng = mersenne(0);
-    const distribution = uniformIntDistribution(from, to);
-
-    const values: number[] = [];
-    for (let idx = 0; idx !== 10; ++idx) {
-      const [v, nrng] = distribution(rng);
-      values.push(v);
-      rng = nrng;
-    }
-    expect(values).toMatchSnapshot();
-  });
 });
+
+// Helpers
+
+const settingsArbitrary = fc.record(
+  {
+    from: fc.integer(),
+    gap: fc.integer({ min: 0, max: 0xffffffff }),
+    rangeRandom: fc.integer({ min: 0, max: 0xffffffff }),
+    ctx: fc.context(),
+  },
+  { withDeletedKeys: false }
+);
+
+type SettingsType = typeof settingsArbitrary extends fc.Arbitrary<infer U> ? U : never;
+
+function mockInternals(settings: SettingsType) {
+  const { uniformIntDistributionInternal } = mocked(UniformIntDistributionInternalMock);
+
+  const { from, gap, rangeRandom, ctx } = settings;
+  const to = from + gap;
+  const rng = buildUniqueRng();
+  const outputs: [number, RandomGenerator][] = [];
+  uniformIntDistributionInternal.mockImplementation((rangeSize) => {
+    const out = [rangeRandom % rangeSize, buildUniqueRng()] as [number, RandomGenerator];
+    ctx.log(`uniformIntDistributionInternal(${rangeSize}) -> [${out[0]}, <rng>]`);
+    outputs.push([...out]); // we clone it, nothing forbid caller to mutate it
+    return out;
+  });
+
+  return { from, to, rng, outputs, uniformIntDistributionInternal };
+}
---
+++ test/unit/distribution/internals/UniformIntDistributionInternal.spec.ts
@@ -0,0 +1,117 @@
+import * as fc from 'fast-check';
+
+import { uniformIntDistributionInternal } from '../../../../src/distribution/internals/UniformIntDistributionInternal';
+import mersenne from '../../../../src/generator/MersenneTwister';
+import RandomGenerator from '../../../../src/generator/RandomGenerator';
+
+class NatGenerator implements RandomGenerator {
+  readonly current: number;
+  constructor(current: number) {
+    this.current = current % 0x80000000;
+  }
+
+  next(): [number, RandomGenerator] {
+    return [this.current, new NatGenerator(this.current + 1)];
+  }
+  min(): number {
+    return 0;
+  }
+  max(): number {
+    return 0x7fffffff;
+  }
+}
+
+class ModNatGenerator implements RandomGenerator {
+  constructor(readonly current: RandomGenerator, readonly mod: number) {}
+
+  next(): [number, RandomGenerator] {
+    const [v, nrng] = this.current.next();
+    return [Math.abs(v % this.mod), new ModNatGenerator(nrng, this.mod)];
+  }
+  min(): number {
+    return 0;
+  }
+  max(): number {
+    return this.mod - 1;
+  }
+}
+
+const MAX_RANGE: number = 1000;
+
+describe('uniformIntDistributionInternal', () => {
+  it('Should always generate values within the range [0 ; rangeSize[', () =>
+    fc.assert(
+      fc.property(fc.nat(), fc.integer({ min: 1, max: MAX_RANGE }), (offset, rangeSize) => {
+        const [v, nrng] = uniformIntDistributionInternal(rangeSize, new NatGenerator(offset));
+        return v >= 0 && v < rangeSize;
+      })
+    ));
+  it('Should be able to generate all values of the range [0 ; rangeSize[', () =>
+    fc.assert(
+      fc.property(fc.nat(), fc.integer({ min: 1, max: MAX_RANGE }), fc.nat(), (offset, rangeSize, targetOffset) => {
+        const target = targetOffset % rangeSize;
+        let rng: RandomGenerator = new NatGenerator(offset);
+        for (let numTries = 0; numTries < 2 * rangeSize; ++numTries) {
+          const [v, nrng] = uniformIntDistributionInternal(rangeSize, rng);
+          rng = nrng;
+          if (v === target) {
+            return true;
+          }
+        }
+        return false; //twice the length should always be enough (+1 to avoid length = 0)
+      })
+    ));
+  it('Should be evenly distributed over the range [0 ; rangeSize[', () =>
+    // NOTE:
+    // >  Actually this property is true for any rangeSize >= 1 such that
+    // >  there exists an N >= 1
+    // >    where RNG_RANGE_SIZE ** N >= rangeSize and RNG_RANGE_SIZE ** N <= Number.MAX_SAFE_INTEGER
+    // >    with RNG_RANGE_SIZE = rng.max() - rng.min() + 1
+    fc.assert(
+      fc.property(
+        fc.nat(),
+        fc.integer({ min: 1, max: MAX_RANGE }),
+        fc.integer({ min: 1, max: 100 }),
+        (offset, rangeSize, num) => {
+          let buckets = [...Array(rangeSize)].map(() => 0);
+          let rng: RandomGenerator = new NatGenerator(offset);
+          for (let numTries = 0; numTries < num * rangeSize; ++numTries) {
+            const [v, nrng] = uniformIntDistributionInternal(rangeSize, rng);
+            rng = nrng;
+            buckets[v] += 1;
+          }
+          return buckets.every((n) => n === num);
+        }
+      )
+    ));
+  it('Should be able to generate values larger than the RandomGenerator', () =>
+    fc.assert(
+      fc.property(fc.integer(), fc.integer({ min: 2, max: 0x7fffffff }), (seed, mod) => {
+        let rng: RandomGenerator = new ModNatGenerator(mersenne(seed), mod);
+        expect(rng.max() - rng.min() + 1).toBeLessThan(Number.MAX_SAFE_INTEGER);
+
+        for (let numTries = 0; numTries < 100; ++numTries) {
+          const [v, nrng] = uniformIntDistributionInternal(Number.MAX_SAFE_INTEGER, rng);
+          rng = nrng;
+          if (v > rng.max()) {
+            return true;
+          }
+        }
+        return false;
+      })
+    ));
+  it('Should be able to generate values outside bitwise operations', () =>
+    fc.assert(
+      fc.property(fc.integer(), fc.integer({ min: 2, max: 0x7fffffff }), (seed, mod) => {
+        let rng: RandomGenerator = new ModNatGenerator(mersenne(seed), mod);
+        for (let numTries = 0; numTries < 100; ++numTries) {
+          const [v, nrng] = uniformIntDistributionInternal(Number.MAX_SAFE_INTEGER, rng);
+          rng = nrng;
+          if (v > 0xffffffff) {
+            return true;
+          }
+        }
+        return false;
+      })
+    ));
+});
