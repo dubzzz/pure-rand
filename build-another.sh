b876cc11103a8b2f6b88e73916a4f3d2da15fbf6
Nicolas DUBIEN <github@dubien.org>
2020-10-29 08:59:10 +0100

    Add build-another script

---
+++ build-another.sh
@@ -0,0 +1,26 @@
+# Forbidden if something has not been commited
+if ! git diff-index --quiet HEAD --; then
+    echo "Please commit your local changes!"
+    exit 1
+fi
+
+# Extract details concerning the current environnment
+rawCurrentBranch="$(git branch | grep "*")"
+currentBranch="$(expr substr "${rawCurrentBranch}" 3 1000)"
+
+# Move to commit and build it
+commitId=$1
+if [ -z "$commitId" ]
+    echo "Please provide a commit id!"
+    exit 1
+fi
+target=$2
+if [ -z "$target" ]
+    target="es6"
+fi
+git checkout "${commitId}"
+yarn
+./node_modules/typescript/bin/tsc --target "${target}" --outDir "lib-${commitId}/"
+
+# Move back to original location
+git checkout "${currentBranch}"
