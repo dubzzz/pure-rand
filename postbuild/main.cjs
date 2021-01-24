9aa206393d6aca1730a339c0bcbecae6496504ab
Nicolas DUBIEN <github@dubien.org>
2021-01-24 20:53:02 +0100

    Add commitHash

--- postbuild/main.cjs
+++ postbuild/main.cjs
@@ -10,7 +10,7 @@ const replace = require('replace-in-file');
 const options = {
   files: 'lib/**/*.js',
   from: [/from '\.(.*)(?<!\.js)'/g, /from "\.(.*)(?<!\.js)"/g],
-  to: ["from '.$1.js'", 'from ".$1.js"']
+  to: ["from '.$1.js'", 'from ".$1.js"'],
 };
 
 const results = replace.sync(options);
@@ -24,6 +24,9 @@ for (const { file, hasChanged } of results) {
 // Fill metas related to the package
 
 // eslint-disable-next-line
+const commitHash = getCommitHash();
+
+// eslint-disable-next-line
 fs.readFile(path.join(__dirname, '../package.json'), (err, data) => {
   if (err) {
     // eslint-disable-next-line
@@ -35,8 +38,8 @@ fs.readFile(path.join(__dirname, '../package.json'), (err, data) => {
 
   const commonJsReplacement = replace.sync({
     files: 'lib/pure-rand-default.js',
-    from: [/__PACKAGE_TYPE__/g, /__PACKAGE_VERSION__/g],
-    to: ['commonjs', packageVersion]
+    from: [/__PACKAGE_TYPE__/g, /__PACKAGE_VERSION__/g, /__COMMIT_HASH__/g],
+    to: ['commonjs', packageVersion, commitHash],
   });
   if (commonJsReplacement.length === 1 && commonJsReplacement[0].hasChanged) {
     // eslint-disable-next-line
@@ -45,11 +48,30 @@ fs.readFile(path.join(__dirname, '../package.json'), (err, data) => {
 
   const moduleReplacement = replace.sync({
     files: 'lib/esm/pure-rand-default.js',
-    from: [/__PACKAGE_TYPE__/g, /__PACKAGE_VERSION__/g],
-    to: ['module', packageVersion]
+    from: [/__PACKAGE_TYPE__/g, /__PACKAGE_VERSION__/g, /__COMMIT_HASH__/g],
+    to: ['module', packageVersion, commitHash],
   });
   if (moduleReplacement.length === 1 && moduleReplacement[0].hasChanged) {
     // eslint-disable-next-line
     console.info(`Package details added onto module version`);
   }
 });
+
+// Helpers
+function getCommitHash() {
+  const gitHubCommitHash = process.env.GITHUB_SHA && process.env.GITHUB_SHA.split('\n')[0];
+  if (gitHubCommitHash) {
+    // eslint-disable-next-line
+    console.info(`Using env variable GITHUB_SHA for the commit hash, got: ${gitHubCommitHash}`);
+    return gitHubCommitHash;
+  }
+  if (process.env.EXPECT_GITHUB_SHA) {
+    if (!gitHubCommitHash) {
+      // eslint-disable-next-line
+      console.error('No GITHUB_SHA specified');
+      process.exit(1);
+    }
+  }
+  const out = execSync('git rev-parse HEAD');
+  return out.toString().split('\n')[0];
+}
