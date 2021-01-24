f4a8f0b6a9689a808b3c5c79ccded67039c1c7fb
Nicolas DUBIEN <github@dubien.org>
2021-01-24 21:02:14 +0100

    Fix cjs/esm tests

--- test/esm/run.sh
+++ test/esm/run.sh
@@ -13,18 +13,14 @@ NODE_MINOR=$(node --version | cut -d. -f 2)
 
 if [ $NODE_MAJOR -gt 13 ] || [ $NODE_MAJOR -eq 13 ] && [ $NODE_MINOR -ge 2 ] || [ $NODE_MAJOR -eq 12 ] && [ $NODE_MINOR -ge 18 ]; then
     cd node-with-import
-    yarn --frozen-lockfile --ignore-engines
-    yarn link "pure-rand"
-    yarn dry
-    yarn start
+    yarn run dry
+    yarn run start
     diff expectedResult.txt out.txt
     cd ..
 fi
 
 cd node-with-require
-yarn --frozen-lockfile --ignore-engines
-yarn link "pure-rand"
-yarn dry
-yarn start
+yarn run dry
+yarn run start
 diff expectedResult.txt out.txt
 cd ..
