9123c25cf0ea1cac57f140c3d7c84b8ef37f6fd0
Nicolas DUBIEN <github@dubien.org>
2021-06-07 20:19:36 +0200

    ✨ Migrate xoroshiro to new API

--- src/generator/XoroShiro.ts
+++ src/generator/XoroShiro.ts
@@ -1,9 +1,9 @@
-import { RandomGenerator } from './RandomGenerator';
+import { RandomGeneratorWithUnsafe } from './RandomGenerator';
 
 // XoroShiro128+ with a=24, b=16, c=37,
 // - https://en.wikipedia.org/wiki/Xoroshiro128%2B
 // - http://prng.di.unimi.it/xoroshiro128plus.c
-class XoroShiro128Plus implements RandomGenerator {
+class XoroShiro128Plus implements RandomGeneratorWithUnsafe {
   constructor(private s01: number, private s00: number, private s11: number, private s10: number) {}
   min(): number {
     return -0x80000000;
@@ -11,12 +11,15 @@ class XoroShiro128Plus implements RandomGenerator {
   max(): number {
     return 0x7fffffff;
   }
+  clone(): XoroShiro128Plus {
+    return new XoroShiro128Plus(this.s01, this.s00, this.s11, this.s10);
+  }
   next(): [number, XoroShiro128Plus] {
     const nextRng = new XoroShiro128Plus(this.s01, this.s00, this.s11, this.s10);
     const out = nextRng.unsafeNext();
     return [out, nextRng];
   }
-  private unsafeNext(): number {
+  unsafeNext(): number {
     const out = (this.s00 + this.s10) | 0;
     // a = s0[n] ^ s1[n]
     const a0 = this.s10 ^ this.s00;
@@ -32,9 +35,13 @@ class XoroShiro128Plus implements RandomGenerator {
     return out;
   }
   jump(): XoroShiro128Plus {
+    const nextRng = new XoroShiro128Plus(this.s01, this.s00, this.s11, this.s10);
+    nextRng.unsafeJump();
+    return nextRng;
+  }
+  unsafeJump(): void {
     // equivalent to 2^64 calls to next()
     // can be used to generate 2^64 non-overlapping subsequences
-    let rngRunner = new XoroShiro128Plus(this.s01, this.s00, this.s11, this.s10);
     let ns01 = 0;
     let ns00 = 0;
     let ns11 = 0;
@@ -44,22 +51,21 @@ class XoroShiro128Plus implements RandomGenerator {
       for (let mask = 1; mask; mask <<= 1) {
         // Because: (1 << 31) << 1 === 0
         if (jump[i] & mask) {
-          ns01 ^= rngRunner.s01;
-          ns00 ^= rngRunner.s00;
-          ns11 ^= rngRunner.s11;
-          ns10 ^= rngRunner.s10;
+          ns01 ^= this.s01;
+          ns00 ^= this.s00;
+          ns11 ^= this.s11;
+          ns10 ^= this.s10;
         }
-        rngRunner.unsafeNext();
+        this.unsafeNext();
       }
     }
-    rngRunner.s01 = ns01;
-    rngRunner.s00 = ns00;
-    rngRunner.s11 = ns11;
-    rngRunner.s10 = ns10;
-    return rngRunner;
+    this.s01 = ns01;
+    this.s00 = ns00;
+    this.s11 = ns11;
+    this.s10 = ns10;
   }
 }
 
-export const xoroshiro128plus = function (seed: number): RandomGenerator {
+export const xoroshiro128plus = function (seed: number): RandomGeneratorWithUnsafe {
   return new XoroShiro128Plus(-1, ~seed, seed | 0, 0);
 };
