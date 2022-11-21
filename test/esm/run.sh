f6b318532b965d56a6a0662b6c540318ae519da5
Nicolas DUBIEN <github@dubien.org>
2022-11-21 08:38:49 +0100

    Switch scripts to v3

--- test/esm/run.sh
+++ test/esm/run.sh
@@ -13,14 +13,14 @@ NODE_MINOR=$(node --version | cut -d. -f 2)
 
 if [ $NODE_MAJOR -gt 13 ] || [ $NODE_MAJOR -eq 13 ] && [ $NODE_MINOR -ge 2 ] || [ $NODE_MAJOR -eq 12 ] && [ $NODE_MINOR -ge 18 ]; then
     cd node-with-import
-    yarn run dry
-    yarn run start
+    yarn dry
+    yarn start
     diff expectedResult.txt out.txt
     cd ..
 fi
 
 cd node-with-require
-yarn run dry
-yarn run start
+yarn dry
+yarn start
 diff expectedResult.txt out.txt
 cd ..
