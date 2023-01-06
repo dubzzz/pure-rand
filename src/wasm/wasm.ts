2341bed19ed8034a7ada56e0871e1f9ed669cd56
Nicolas DUBIEN <github@dubien.org>
2023-01-06 17:02:30 +0000

    add wasm.js support

--- src/wasm/wasm.ts
+++ src/wasm/wasm.ts
@@ -1,5 +1,5 @@
 import { RandomGenerator } from '../generator/RandomGenerator';
-import { xoro_shiro_128_plus } from './generated/pure_rand_rs';
+import { xoro_shiro_128_plus, unsafe_uniform_int_distribution } from './generated/pure_rand_rs';
 
 type WasmRandomGenerator = Omit<RandomGenerator, 'clone' | 'next' | 'jump'> & {
   clone: () => WasmRandomGenerator;
@@ -10,4 +10,8 @@ type WasmRandomGenerator = Omit<RandomGenerator, 'clone' | 'next' | 'jump'> & {
 export function xoroShiro128Plus(seed: number): RandomGenerator {
   const rng: WasmRandomGenerator = xoro_shiro_128_plus(seed);
   return rng as RandomGenerator;
+}
+
+export function unsafeUniformIntDistribution(from: number, to: number, rng: RandomGenerator): number {
+  return Number(unsafe_uniform_int_distribution(BigInt(from), BigInt(to), rng));
 }
