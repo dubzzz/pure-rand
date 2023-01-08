fd03178145b930984c17843a1edfa6567cedbf14
Nicolas DUBIEN <github@dubien.org>
2023-01-08 16:46:57 +0000

    🔥 Clean useless variable

--- src/distribution/internals/UnsafeUniformIntDistributionInternal.ts
+++ src/distribution/internals/UnsafeUniformIntDistributionInternal.ts
@@ -10,10 +10,9 @@ export function unsafeUniformIntDistributionInternal(rangeSize: number, rng: Ran
 
   // Range provided by the RandomGenerator is large enough
   if (rangeSize <= NumValues) {
-    let nrng = rng;
     const MaxAllowed = NumValues - (NumValues % rangeSize);
     while (true) {
-      const out = nrng.unsafeNext();
+      const out = rng.unsafeNext();
       const deltaV = out - MinRng;
       if (deltaV < MaxAllowed) {
         return deltaV % rangeSize; // Warning: we expect NumValues <= 2**32, so diff too
@@ -31,12 +30,11 @@ export function unsafeUniformIntDistributionInternal(rangeSize: number, rng: Ran
   }
   const MaxAcceptedRandom = rangeSize * Math.floor((1 * FinalNumValues) / rangeSize);
 
-  let nrng = rng;
   while (true) {
     // Aggregate mutiple calls to next() into a single random value
     let value = 0;
     for (let num = 0; num !== NumIterations; ++num) {
-      const out = nrng.unsafeNext();
+      const out = rng.unsafeNext();
       value = NumValues * value + (out - MinRng); // Warning: we overflow may when diff > max_safe (eg: =max_safe-min_safe+1)
     }
     if (value < MaxAcceptedRandom) {
