16887c551a442864704e753eea261e2587563b8f
Nicolas DUBIEN <github@dubien.org>
2023-01-09 21:19:03 +0100

    Update src/distribution/internals/UnsafeUniformIntDistributionInternal.ts

--- src/distribution/internals/UnsafeUniformIntDistributionInternal.ts
+++ src/distribution/internals/UnsafeUniformIntDistributionInternal.ts
@@ -8,10 +8,7 @@ import { RandomGenerator } from '../../generator/RandomGenerator';
 export function unsafeUniformIntDistributionInternal(rangeSize: number, rng: RandomGenerator): number {
   // Range provided by the RandomGenerator is large enough,
   // given rangeSize <= 0x100000000 and RandomGenerator is uniform on 0x100000000 values
-  if (rangeSize === 1) {
-    return 0;
-  }
-  const MaxAllowed = rangeSize !== 2 ? ~~(0x100000000 / rangeSize) * rangeSize : 0x100000000;
+  const MaxAllowed = rangeSize > 2 ? ~~(0x100000000 / rangeSize) * rangeSize : 0x100000000;
   let deltaV = rng.unsafeNext() + 0x80000000;
   while (deltaV >= MaxAllowed) {
     deltaV = rng.unsafeNext() + 0x80000000;
