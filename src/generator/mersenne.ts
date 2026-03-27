0a7874c31b3ca5dd318b9aaeca05b8e763411fac
Nicolas DUBIEN <github@dubien.org>
2026-03-27 08:38:41 +0000

    comment

--- src/generator/mersenne.ts
+++ src/generator/mersenne.ts
@@ -13,15 +13,16 @@ const L = 18;
 const MASK_LOWER = 2147483647; // = 2 ** R - 1 (with R = 31)
 const MASK_UPPER = 2147483648; // = 2 ** R
 
-// Derived from https://github.com/numpy/numpy/blob/57fbc869cb10a65da0793ed3aebe00e7f595c0b1/numpy/random/src/mt19937/mt19937-jump.h#L26
-// Compressed as base-92 encoding: 5 printable ASCII characters per uint32 coefficient (little-endian).
-// Alphabet: ASCII 33–126 excluding 39 (') and 92 (\), giving 92 symbols (92^5 > 2^32).
-// To regenerate from the numpy uint32 array `coefs` (624 entries):
-//   alphabet = Array.from({length:94},(_,i)=>i+33).filter(c=>c!==39&&c!==92);
-//   coefs.map(v => Array.from({length:5},()=>{ const r=v%92; v=Math.floor(v/92); return String.fromCharCode(alphabet[r]); }).join("")).join("")
+// jump() uses these coefficients to skip 2^128 values at once, creating independent streams.
+// Each char encodes 6 bits of the jump polynomial from numpy's mt19937-jump.h.
+// To regenerate from numpy's poly_coef (uint32[]) array:
+// >  const bits = poly_coef.flatMap(v => Array.from({length: 32}, (_, i) => (v >>> i) & 1));
+// >  const encoded = String.fromCharCode(...Array.from({length: Math.ceil(bits.length / 6)}, (_, i) => {
+// >    const v = bits.slice(i * 6, i * 6 + 6).reduce((a, b, j) => a | (b << j), 0);
+// >    return v + 48;
+// >  }));
 const JUMP_COEFS =
-  'rjtu<ZS5OLisQtA&_`DI;Fx*+Sml{![GC@XrDf[BmlW#3[`UiNS88UWPh)^#b10_0ut<`X~}](Kcqa?#aqx".)v0?OY7_.B>cL_^^SOG!MI7$5u~<)W%_L=F+>b.MizgFZFMZ~5YM|cT5P0&Z@^f&L7/<c)-02/TUmxURvE7b2;FXDCn.hGC6pob6tK%b8B@*K,}IjIZcdqqKCCNCQO3CF@d}t[A,v!!Ed:=?R.,b{="[$-=F8n>6wT,4[g23F)VlWfNd{PdD"k&w(e{2GQpTGvF.;+(Gx^l2Xa}9e*6qN(R{{#+=k&:r?.@1#QxnOu&8oi<T"e7@5AHXDL5?%[EA&wO)_"+w!*3#|*&M"9>fPU,Er(,a1a-O7Jg1_@:[CMO%~5.c%}C,"D]n~]S&N~e01:<M(79MD,&XtB)X5QEFfkBgA37?TH:!<SHai_IElWVl(mqY}P7ML?T>z8uGV,2P+nx=z@(8sS870WkU<P/bN5U0EV|(&LH`k6bM;%lG30U#:I/saI[MhX^R:~3IH-yt&TtviqZ/`2AR,hJnJC)`bZau}9PnomqFqN5F,h`D}K)|A:YrT[w/u*>gQ,p)5?dM#SN(z<q!Xsa`)-nvnVx2FlNIS{<3UVHCF%^?O#E%"A:!V2zZW|*o?FBnzQA+.K9T9J[;Jc)/@@9Kr<8~S`Z2U?v/`.)6Bq9(_48;@H+7hGx8xv>?5M+(h@5S/3Nq,`9Q,G6wDTH5=G7kZ%MjEaPRAW7_7tZ&9[r>Vo2;3PoTbs{BTj/LL8JNY[Ja5DGP,TX8^<Q7T$VKxU3w|$e<M2e{4mkUC)1m7tL|=Qs^0.fj534M.Dwj8Q.v*Kg%S+k]U&xB>Epp/VL3?Q"HO:G[I0Qe2EUPDaO$9k:9vg@8V!D]0/EO!-BH`!]<JBj3B8#G90wJoN7.8;XJ="I`N*3Ue7rUppTkNItBS:$~$K!Z,.{E|I2<zMyO3I)0ZFgwQ8;2>5*%cM)+[@E:3=$,/5B5r+?Q&"l2[2%s&~Fd:0*{IRJ^.*ODW55zT(@RxDh6EKYj56;^7<`j#%2Xs76(~G?{Fvx^;}fU<jur]%D1.1"3PDLT-;"D(=QykA,bw.B_ZCNOkd^4E#DkJXi3{J]WgmwMGw;+BkdK<Xt>s%4OAfZ#aw}FQAZug.3je2M~wM,6)N0q<F<ye%zp?n2$LE8HB%T<J0>T[Cg)8/49>0SDWw=]!oYGe)*ils"HxkJ@?Xe(J7X9("%-I[L5$q-E(Lp8]vD$K:LalM3T:GEKBIUe5iLd8H5f$>@SF8v*N^B[/?|_OLv~eIMeaa&=*!zxI(O73!F*ybH})a]RTs~JNKdD7#pR3B5rZ]YHLIz6Ear<w3FoX!Sn}OX[&q0gKW&^$@L0d%MS(3=I2@C}<d^SY"(aKv9|vFYE{Ua%P,M]7>sRsw?:4"@A6A&4Ps4Y/O7(=2-e4shOAwvf)aO2eCbQT[8myVBCBAF-APv0(0c,tK=MTcZARxcz2k~Ip%Th_LR%TJ[Y.|n>Z;%>#$q"j5ZclwGVTTnZ?FH.iBn+W+!m)Y/6ze@T+Z4s$K8_5oNd+aXI`E.n,_TV28ph8u,h0jiOBA@z@j%|N@4Cpw8u/&|!eAU/%2GgZWH=+fFPWy1]=a>NF)N7E5osf>@4?Lj2H(l`G8*qu#A9oyXm;e~U|!"0Paqjf0H6hlZEW9a:Q)~B^4",nCQ6Z*N[BaGVRtWR19^+9&,I;lMW+^y+x#x(Pot^~W46J:2ndunDI@[o"5!XY8w_k@=e2ZAUr8`{T~*sVAFXos7(3%[@m}lBLj^clKUx#uLt8kG.=AF51R#9J;LUt,XXkMj9U,_X.Y:?n]*$#WWD$xE+N}V;AGX2rW|Kt$2Xs*0]t>DS];sur9p3mlA%!^SNi|ChJ?(S.0I,9nTbR<xJasq(Sj(lO$H?<"24X?R2[&v-Vccm;OM2zpRvfQ`KP;dI>C.S$P4)L&Hg/BT9!OEo:.~kM/cWs9$d?B^Wm%b?4,p4~()E/3Kv-/g<4BgSQ1XN?*#J$^@8Xq4Qkte[)s=U8S7vhKBu4"<A^J7fY$[:{E`?VwZ>nKgF_3!_MW<[l.`J1sD%Q7VYV?dSA`J}-1xIyV2>OXB"]U^_L{*+h>*Ur-Ce$}S#Q4~n?/;tX:z^yy7bQY~1H&~<0K2<gX?Apj8P(Ol5`@.z%?S;$D[DQZ8di1/8d*wV0_6nR/Z$qU*#$O&^!GiT&G-u[@$pT:,2K8p>>~-7"[_[m7z=#Q?]_vaTA#d6*BBWUM+PQ4E5`@AHWO_cE"/nH.WC`kHt^*}*9o@_P}_a:!{z0&R??[:PBtjuRmRoW7*"666hWFE)6Q6DHZV}t@6MzT"M:"gD!y3N2&;eCF%]<f]KMY|67%0OKj$:b]upVY0iW"r/mRfcWHN;@Cj?{!AwZ0Y0(cylY[~Kf7_GwF1RVou$0-vpG,x3ZB}&aBB&I[Y/P"*c.$-pE9Hqho<<kwa>":][NC<7uXeq;)0}w/hP7*jW"kub"/g%9=?ZV[|QOjcTM?HvP8|;(qAFGV:]<Gn&Z^<`;?!u.R5$xIT7TQliCL(yF9&!<8J|"n?!?9u;-A1`QUgu*i(<]YMAt"*SK/rR#%cjgq#T>^7*%Kl1LKO5EDM#.Q5rt}3#S<T%^X#uLD<E*f*A#UzQ!nLO(s}/;0ccfGI`D-o$8]Nw@l~O+U-498)N^9"BF%U^4_Q]CAw5Jw;@g9313C35<c?x10J=5g9d_0@K.X2;<Sh_:9HDL`;,T$ZY|:X;9pccE&AwxC$pK8]&9?KcFiHOXSmo[@>:BvDA<TZ&HB#T!E?9XiCb7Ea3adnA)+w`<CWo;@<Z!!5Y]eW>@;,R$Byb|Z0zb)M=;z@xI#6|bXn`FaLR"K=/Q{ns<}wZJM2f[PWrDY{5qWx0?!XK!L+1{aY0{xFQR,Fq%$D_a"rFM,LyGT@5;F17%TcQ:$/dGmA*;3G%nl~_YsAV=3"bR@CH5@8X,Fjb={u0h+a!4!LjBk67g2.=-v^w-My7D7&f:pyHpvXvD9e?DVE_7g^3zK}B%@D28EV3@9tp[&(!q}a?TE`z=94tMHYT||"@=yFKJ^(s@FI%q/J3,.=dH{hG?+41EQSk&=u$f-Y@d`y%IHa-@b44H*!!!!!';
-
+  'SUSgbA\\W`E[]KN2RUSo8XVU?HKBFRl11E\\KoWOg5B]XEWG;BE;1:oVK[`B^Z9Qd23^XTnhL>]Unda4f[X;_j9H5QD=cN<5H`3bW>9bk1mjoI2fK0obmAAINOV:>Mek_V9dd<hZ\\gC3?Fm7FEk07QH_3PLm^@?^i\\QMkgP<]oLHmFnlecg5F@7U^@4jhZ?WZS0k@GHehmM36:5^9;>Hmm`co>k:KOSkSbIINb1VFf>LXgP>GUAQTD>Ci>XMGkUflLlb?_FaFUk@?5N7i70@;1o68ah@I<HFH7R2^J:G][Gf962ITWID9GWK8ElD2G5=DcHcL]cA]P2n7A=[<bInM;IHDQnJMReRXDWbVldnGEIPij`E08Xdci3@0c:IBbD4:Nk]?lEN9j^;T`0blZX7eiWE8c`<ak7j05FZi>AjUDh?M1B?^??FAXKThf<aBOXZf7jXYGK>R<;NHk3S9YhM7STJ6`:MIE`S@7298X8W>PNK=@;lLX<i\\TXLL<W@X[X54H]in8M;;n?kkQbajgAMY=Tf9b;ZKf0QUB2FHYWfnfkDoU9YkcLd95T>lK6GM1YL\\lid:J>KYS=iJ]Y>QlF>?R5_[5QeYC=66;A32Ac>OHk_ne^0g>bK:g;KFPgbGUcPR_Z=TX3H9d03bKZ2IhEPKBo>LSGWd0iFdV8C<Y:<>T[O6lC\\blaZ>GoAYP4clf^j1IfnZJ]QeDe2X<HE[LJNWnaCg[P]Co^:IRbWPY?97UePBlZNNHY6LOBM8P>=h?Ye:_f_Sb9Ki5GDYBF4dWeMfdg^ccPllNWM7G1\\UMdoYeOOD5^e@foA22G?ADYo5:FVG[bWo96;>3kc_c1Ab30>30;1@4F8g2hY?DJ4[LOL;ZLLKo2]jo>[KMDUcR279N_kF=3WL@Dd620bMTdA\\U9k``ef2iD9JgJ8CZBHS>F^Uk;<laeaeHS<15OSeS`PcSSKBRBFY]aQ=EgUXGNg=?d56`KA@2BejY0^[_DCX`L=CGMT=BW^6S1i@2ATBVk>3_ocRA>2U:4GPQ6o>5jX2HIcV3S@On6KB<[SKB?FC_AAji9agbBFkAi\\;4I\\UJ]c36Ub@[;gQACVGY<V]SDJBUU]La\\_a@JdOO8gm2T0DJMa:8Hf7>E]noQ[1Kambn??QQ]S?1i0oMGOijb\\aGY6lQ^CJ?9bFle8<eH4UUjBINX;n8@VOA5ah^URV49B[A?ONHhHAC1J5;;h0SXYlG^0W=eJdHh^K4SGe=1HZLLam;D<Q3AOdbPcdX``82\\jo0En8jRVGC73WMCF9`d:0heS?80?C188cSn7H9<daZ]MgS4Pb^1HkA:1PU^5>^h_g[RQV@PnYBRI_]`B]Bh@Uk03eXGY`I16L76H28X`R>IROMeNVUdU[:lghLhPCQZ:4a<30YBZCYnXe[?;jc8gKI2QH2MjnWBm4nGCZW`aVU2a;P<AMI25mlW_Nm][2?b6o851X@lAm]YZ`bWRF1g<Ga:T]1NXH5Veja5P9S9>:aNg:Th1Y=5o78K>LQ8hW@5S?I83Lk5Xk;j5@I3o[d:4RjE^oS30:WP9gC\\i8aSI>QRE@4lP:7lDg8g2`Ql[2I8aBU\\BQ?B4_clL9Q]S;^e1Ob5[>3JER2`c7B=o]fPOWO<DGi;Niba1PoWPPE_Q3aX0OB0mZej\\f_M[J4Y6]1`h2MkF[GiW8Q^d3^_=<I1N2Q7]2<2j[iP7V3V821FaI]A`93bC^Z\\G=WJ;^Ih?B97_iIF@\\Dl<eK1je8SNTWo_=XFMZH<<JYLZ^YQMPgYOV45K_:]kSI8^XlO0]GY=VUfe_C_F6TOcoAlVUH:o=WhhT@K`2KFhe<3\\KXQ>W:M?S_4S5d@J^`[AGA7@3]DnGSCO`\\?E8HT75^d9\\:m\\m1egIfk8cd6bD9\\eU8\\n[Pb0Cgd^S0n9kGJHb]i5XodlKHc34Fhi9K>0U5WK`>7Ff2^KL=WC6:kc?e5C^a1T1:4:^S5flXlGNIj08AfO?Dh7T7dWO>E]NI9?ob7B7P_h[4TEP[EU;GllFTnSmg9:\\[N]<SAoKP_kPlG56A:I3T6EG8Hj=XnUE`KT5U@OQ?]7[N^MFN1_NU4KO_3::Le`8IL9[1Z1H1;V3SC\\N3]S@4U[F2mhT5dYM7[4Pg_Na0_8WTH0`bgceQS8]EG;XgD4Ib4iLTP@kE79Mn>AYRJA1U5^Blhgno:aHVYc03c3J0Vc9FjEV^M75Zfd8kVC9>iJDk`AJ[6f7DK2D^DL\\AX:6b5h31XH;RQB\\N<ZSM=J;6L[`UW^eOIFc1Y]6_dfedIe4ARh72mTXC0WND_IDHVCZRDE0eODARCEETQI5TPUQE=jEH5bS?LP[ai`F5ABRYDo2o\\@=]GT?_9;hc4Lm:\\SF9k1T<0E@fX9BG[]g0nY7k[Qmi@la8`PF0j6@Q?Ii7bLkXQ<lLHf]`:DCh@9JY5>hEVLTdhL\\b1EB0lLh<=WaO@F<;8g<d:@e:LA:cFIEdmQh7hNHfSRToW?8N4:Z1K;XEDRO;OIDh<UdVln?bjgL>?VE98[B\\K<BVjkG8LiSX;fb>jf?DUK00Aih<WY6QD6cEnHBZ8iN_fd<G8Ci`11RUW2QlW]IXV:m?;J0GXXHfGNQ>:D`=fLPbO?VOTEYLj^cNj5PM>jKB5HVjJ4U7lXaTQNL9<@\\1`m\\Ug@VQHd7>jW=ca0`miF7;N0F=GjoQ`RFchKMGTmn8cF@Oh4GGCm7m2`U9j93Tb>=kSERjE_J939F01I1;`<ijk_=_Vn=7RVAI6fnQI5KlF:C44bN<<8K=Z<2TP<1]5?<dB>^LA=ebloE2Y2:9lkh0\\<YKbHD97iE`<C5oj^1>X:??`H6BXF2hG1Q[dF0Q=>W=J?7C\\k1T?<;R44oW?1hY^G8Zm]ZKnfOf0eCFYo6?=D8?<`6HU5SXh1;=:23LmV_FSi;OJfV<^?GkIDPISeHg1LaGE:V3Y3K3H<QJfbY?=;ldZRhnhQT_mGXDLFXXhSONE6Do_0iNZagB:BPGOTH;VhHUTd6LhQm^[;dO]5Hlkg<R:F<Kn\\:I:EGojgWZ2X@SYO_dlH;G8S<>oEKabY`:oU;=JW7ig?S?EYb86b7n8ce\\]IRa]koiWY<RfO;5kUI;7lVeC?[@ZaXDiF04B8R]bg@>O<mQDoUcBLcf^f>m2kMBUloD>Ze@NN^Z11TM`inXYhE_I=kA`:ZF4d\\>`L@;ZP[`ENU5cL[BV6\\Z?Di76:jg3hE6oG6jFc8kP=[GS1;WSedYQW1:U4\\OF32GgmMC<AjO]872bdBb`bKAA?8j78b>T3VfcUB2m4J^CPRU;8dScI]LU]^bBYA5_3:Y0N5i^?200000';
 
 class MersenneTwister implements JumpableRandomGenerator {
   constructor(
@@ -52,22 +53,12 @@ class MersenneTwister implements JumpableRandomGenerator {
     this.index = twistedNext(this.states, this.index); // states and index of this
     // https://github.com/numpy/numpy/blob/57fbc869cb10a65da0793ed3aebe00e7f595c0b1/numpy/random/src/mt19937/mt19937-jump.c#L71-L72
     // We burn: 19935 and 19934 as they result into get_coef(pf, i) == 0
-    // So we start the loop with "19933 - 1" ie word 622 bit 28
-    // Outer loop iterates words, inner loop iterates bits to decode each word only once
-    for (let w = 622; w >= 0; --w) {
-      let coef = 0;
-      for (let j = 0, base = 1; j < 5; j++, base *= 92) {
-        const c = JUMP_COEFS.charCodeAt(w * 5 + j);
-        coef += (c < 39 ? c - 33 : c < 92 ? c - 34 : c - 35) * base;
+    // So we start the loop with "19933 - 1"
+    for (let i = 19932; i > 0; --i) {
+      if ((JUMP_COEFS.charCodeAt((i / 6) | 0) - 48) & (1 << (i % 6))) {
+        addState(this.states, this.index, originalStates, originalIndex);
       }
-      const topBit = w === 622 ? 28 : 31;
-      const bottomBit = w === 0 ? 1 : 0;
-      for (let b = topBit; b >= bottomBit; --b) {
-        if ((coef >>> b) & 1) {
-          addState(this.states, this.index, originalStates, originalIndex);
-        }
-        this.index = twistedNext(this.states, this.index); // states and index of "copy"
-      }
+      this.index = twistedNext(this.states, this.index); // states and index of "copy"
     }
     // https://github.com/numpy/numpy/blob/57fbc869cb10a65da0793ed3aebe00e7f595c0b1/numpy/random/src/mt19937/mt19937-jump.c#L85-L88
     // We fall in the case: get_coef(pf, 0) != 0
